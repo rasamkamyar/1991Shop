e3a7f7623775471536902a83eb44f96eaf56f26e
rasam kamyar <rasamkamyar@gmail.com>
2026-02-27 17:48:24 +0330

    add types

---
+++ src/features/types/types.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  id: number;
+  image: string;
+  title: string;
+  price: string;
+}
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,6 +1,7 @@
+import type { Product } from "../features/types/types";
 import { api } from "./api";
 
-export const getProducts = async () => {
+export const getProducts = async (): Promise<Product[]> => {
   const { data } = await api.get("/products");
   return data ?? [];
 };
